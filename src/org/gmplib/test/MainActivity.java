982c66152824e3950bb436e65115286e5ba97a54
aquick <aquick@alumni.uwaterloo.ca>
2015-12-15 16:58:43 -0500

    Added clear button to UI.  Changed random file initialization.  Added banner with GMP version.

--- src/org/gmplib/test/MainActivity.java
+++ src/org/gmplib/test/MainActivity.java
@@ -19,21 +19,24 @@ import java.io.IOException;
 //import java.lang.reflect.Array;
 import java.util.StringTokenizer;
 
+import org.gmplib.gmpjni.GMP;
+
 public class MainActivity extends Activity implements UI {
 
     private TextView mView;
     private Button mButton;
     private Button mCancel;
+    private Button mClear;
     private PowerManager.WakeLock mWakeLock;
     private RandomNumberFile rng;
     private Object[] tasks;
     private int numTasks;
     private int currTask;
-    private String randfname = "2010-08-16.hex.txt";
+    private String randfname = "2010-03-02.hex.txt";
     private int base = 16;
     private static final String TAG = "MainActivity";
     
-    private void initRandom() throws IOException
+    protected void initRandom() throws IOException
     {
         int n = 0;
         String root = this.getExternalFilesDir(null).getPath();
@@ -57,7 +60,7 @@ public class MainActivity extends Activity implements UI {
         rng.skip(n);
     }
     
-    private void finiRandom() throws IOException
+    protected void finiRandom() throws IOException
     {
         if (rng != null) {
             long consumed = rng.consumed();
@@ -77,6 +80,11 @@ public class MainActivity extends Activity implements UI {
         mView.append("\n");
     }
     
+    public void clearDisplay()
+    {
+        mView.setText("");
+    }
+    
     public void nextTask()
     {
         currTask++;
@@ -88,6 +96,12 @@ public class MainActivity extends Activity implements UI {
             if (mWakeLock != null && mWakeLock.isHeld()) {
                 mWakeLock.release();
             }
+            try {
+                finiRandom();
+            }
+            catch (IOException e) {
+                Log.d(TAG, "EXCEPTION: " + e.toString());
+            }
         }
     }
     
@@ -95,58 +109,67 @@ public class MainActivity extends Activity implements UI {
     {
         currTask = 0;
         numTasks = 0;
-        /***/
-        tasks[numTasks++] = new TDiv_UI_Task(this, this.rng);
-        tasks[numTasks++] = new TDiv_Task(this, this.rng);
-        tasks[numTasks++] = new SqrtRem_Task(this, this.rng);
-        tasks[numTasks++] = new Set_Str_Task(this, this.rng);
-        tasks[numTasks++] = new Set_SI_Task(this, this.rng);
-        tasks[numTasks++] = new Set_F_Task(this, this.rng);
-        tasks[numTasks++] = new Set_D_Task(this, this.rng);
-        tasks[numTasks++] = new Scan_Task(this, this.rng);
-        tasks[numTasks++] = new Root_Task(this, this.rng);
-        tasks[numTasks++] = new Remove_Task(this, this.rng);
-        tasks[numTasks++] = new Primorial_UI_Task(this, this.rng);
-        tasks[numTasks++] = new PPrime_P_Task(this, this.rng);
-        tasks[numTasks++] = new Powm_UI_Task(this, this.rng);
-        tasks[numTasks++] = new Powm_Task(this, this.rng);
-        tasks[numTasks++] = new Pow_Task(this, this.rng);
-        tasks[numTasks++] = new PerfSqr_Task(this, this.rng);
-        tasks[numTasks++] = new PerfPow_Task(this, this.rng);
-        tasks[numTasks++] = new OddEven_Task(this, this.rng);
-        tasks[numTasks++] = new NextPrime_Task(this, this.rng);
-        tasks[numTasks++] = new Mul_I_Task(this, this.rng);
-        tasks[numTasks++] = new Mul_Task(this, this.rng);
-        tasks[numTasks++] = new MFac_UIUI_Task(this, this.rng);
-        tasks[numTasks++] = new LucNum_UI_Task(this, this.rng);
-        tasks[numTasks++] = new LCM_Task(this, this.rng);
-        tasks[numTasks++] = new Jacobi_Task(this, this.rng);
-        tasks[numTasks++] = new Invert_Task(this, this.rng);
-        tasks[numTasks++] = new PopCount_Task(this, this.rng);
-        tasks[numTasks++] = new HamDist_Task(this, this.rng);
-        tasks[numTasks++] = new Get_SI_Task(this, this.rng);
-        tasks[numTasks++] = new Get_D_Task(this, this.rng);
-        tasks[numTasks++] = new Get_D_2Exp_Task(this, this.rng);
-        /***/
-        tasks[numTasks++] = new GCD_UI_Task(this, this.rng);
-        tasks[numTasks++] = new GCD_Task(this, this.rng);
-        tasks[numTasks++] = new Fits_Task(this, this.rng);
-        tasks[numTasks++] = new Fib_UI_Task(this, this.rng);
-        tasks[numTasks++] = new FDiv_UI_Task(this, this.rng);
-        tasks[numTasks++] = new FDiv_Task(this, this.rng);
-        tasks[numTasks++] = new Fac_UI_Task(this, this.rng);
-        tasks[numTasks++] = new Divis_2Exp_Task(this, this.rng);
-        tasks[numTasks++] = new Divis_Task(this, this.rng);
-        tasks[numTasks++] = new Div_2Exp_Task(this, this.rng);
-        tasks[numTasks++] = new Cong_2Exp_Task(this, this.rng);
-        tasks[numTasks++] = new Cong_Task(this, this.rng);
-        tasks[numTasks++] = new Cmp_SI_Task(this, this.rng);
-        tasks[numTasks++] = new Cmp_D_Task(this, this.rng);
-        tasks[numTasks++] = new Cmp_Task(this, this.rng);
-        tasks[numTasks++] = new CDiv_UI_Task(this, this.rng);
-        tasks[numTasks++] = new Bin_Task(this, this.rng);
-        tasks[numTasks++] = new AddSub_Task(this, this.rng);
-        tasks[numTasks++] = new AddOrSubMul_Task(this, this.rng);
+        try {
+            GMP.init();
+            display(getString(R.string.testing) + " " + GMP.getVersion());
+            initRandom();
+            /***/
+            tasks[numTasks++] = new TDiv_UI_Task(this, this.rng);
+            tasks[numTasks++] = new TDiv_Task(this, this.rng);
+            tasks[numTasks++] = new SqrtRem_Task(this, this.rng);
+            tasks[numTasks++] = new Set_Str_Task(this, this.rng);
+            tasks[numTasks++] = new Set_SI_Task(this, this.rng);
+            tasks[numTasks++] = new Set_F_Task(this, this.rng);
+            tasks[numTasks++] = new Set_D_Task(this, this.rng);
+            tasks[numTasks++] = new Scan_Task(this, this.rng);
+            tasks[numTasks++] = new Root_Task(this, this.rng);
+            tasks[numTasks++] = new Remove_Task(this, this.rng);
+            tasks[numTasks++] = new Primorial_UI_Task(this, this.rng);
+            tasks[numTasks++] = new PPrime_P_Task(this, this.rng);
+            tasks[numTasks++] = new Powm_UI_Task(this, this.rng);
+            tasks[numTasks++] = new Powm_Task(this, this.rng);
+            tasks[numTasks++] = new Pow_Task(this, this.rng);
+            tasks[numTasks++] = new PerfSqr_Task(this, this.rng);
+            tasks[numTasks++] = new PerfPow_Task(this, this.rng);
+            tasks[numTasks++] = new OddEven_Task(this, this.rng);
+            tasks[numTasks++] = new NextPrime_Task(this, this.rng);
+            tasks[numTasks++] = new Mul_I_Task(this, this.rng);
+            tasks[numTasks++] = new Mul_Task(this, this.rng);
+            tasks[numTasks++] = new MFac_UIUI_Task(this, this.rng);
+            tasks[numTasks++] = new LucNum_UI_Task(this, this.rng);
+            tasks[numTasks++] = new LCM_Task(this, this.rng);
+            tasks[numTasks++] = new Jacobi_Task(this, this.rng);
+            tasks[numTasks++] = new Invert_Task(this, this.rng);
+            tasks[numTasks++] = new PopCount_Task(this, this.rng);
+            tasks[numTasks++] = new HamDist_Task(this, this.rng);
+            tasks[numTasks++] = new Get_SI_Task(this, this.rng);
+            tasks[numTasks++] = new Get_D_Task(this, this.rng);
+            tasks[numTasks++] = new Get_D_2Exp_Task(this, this.rng);
+            /***/
+            tasks[numTasks++] = new GCD_UI_Task(this, this.rng);
+            tasks[numTasks++] = new GCD_Task(this, this.rng);
+            tasks[numTasks++] = new Fits_Task(this, this.rng);
+            tasks[numTasks++] = new Fib_UI_Task(this, this.rng);
+            tasks[numTasks++] = new FDiv_UI_Task(this, this.rng);
+            tasks[numTasks++] = new FDiv_Task(this, this.rng);
+            tasks[numTasks++] = new Fac_UI_Task(this, this.rng);
+            tasks[numTasks++] = new Divis_2Exp_Task(this, this.rng);
+            tasks[numTasks++] = new Divis_Task(this, this.rng);
+            tasks[numTasks++] = new Div_2Exp_Task(this, this.rng);
+            tasks[numTasks++] = new Cong_2Exp_Task(this, this.rng);
+            tasks[numTasks++] = new Cong_Task(this, this.rng);
+            tasks[numTasks++] = new Cmp_SI_Task(this, this.rng);
+            tasks[numTasks++] = new Cmp_D_Task(this, this.rng);
+            tasks[numTasks++] = new Cmp_Task(this, this.rng);
+            tasks[numTasks++] = new CDiv_UI_Task(this, this.rng);
+            tasks[numTasks++] = new Bin_Task(this, this.rng);
+            tasks[numTasks++] = new AddSub_Task(this, this.rng);
+            tasks[numTasks++] = new AddOrSubMul_Task(this, this.rng);
+        }
+	catch (IOException e) {
+            Log.d(TAG, "EXCEPTION: " + e.toString());	    
+            display("EXCEPTION: " + e.toString());
+	}
     }
     
     protected void executeCurrentTask()
@@ -171,6 +194,12 @@ public class MainActivity extends Activity implements UI {
                 mWakeLock.release();
             }
         }
+        try {
+            finiRandom();
+        }
+        catch (IOException e) {
+            Log.d(TAG, "EXCEPTION: " + e.toString());
+        }
     }
 
     @Override
@@ -182,11 +211,10 @@ public class MainActivity extends Activity implements UI {
         mView = (TextView) findViewById(R.id.TextView01);
         mButton = (Button) findViewById(R.id.Button01);
         mCancel = (Button) findViewById(R.id.Button02);
-        mView.append("\n");
+        mClear = (Button) findViewById(R.id.Button03);
         tasks = new Object[100];
         try {
             TestUtil.initprimes(65535);
-            initRandom();
             mButton.setOnClickListener(
                     new View.OnClickListener() {
                         public void onClick(View v)
@@ -202,13 +230,19 @@ public class MainActivity extends Activity implements UI {
                             MainActivity.this.cancelCurrentTask();
                         }
                     });
+            mClear.setOnClickListener(
+                    new View.OnClickListener() {
+                        public void onClick(View v)
+                        {
+                            MainActivity.this.clearDisplay();
+                        }
+                    });
             PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
             mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, TAG);
         }
         catch (Exception e) {
             Log.d(TAG, "EXCEPTION: " + e.toString());
-            mView.append("EXCEPTION: " + e.toString());
-            mView.append("\n");
+            display("EXCEPTION: " + e.toString());
         }
     }
 
@@ -248,6 +282,7 @@ public class MainActivity extends Activity implements UI {
     {
         super.onResume();
         Log.d(TAG, "onResume");
+        /***
         try {
             if (rng == null) {
                 initRandom();
@@ -256,6 +291,7 @@ public class MainActivity extends Activity implements UI {
         catch (IOException e) {
             Log.d(TAG, "EXCEPTION: " + e.toString());
         }
+        ***/
     }
     
     @Override
